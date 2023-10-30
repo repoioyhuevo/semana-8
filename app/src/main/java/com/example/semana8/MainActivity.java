f1c6adcea2f96a9a4459ed9940c391c6b8678f7c
repoioyhuevo <repoioxd99963@gmail.com>
2023-10-30 17:10:21 -0300

    commit semana 8

--- app/src/main/java/com/example/semana8/MainActivity.java
+++ app/src/main/java/com/example/semana8/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        final Bitmap bitmap = loadImageFromNetwork("https://th.bing.com/th/id/R.5f39333ae688750dfba41aaa43ce8e5e?rik=8VD6p6fGbIV8Yg&pid=ImgRaw&r=0");
+                        final Bitmap bitmap = loadImageFromNetwork("https://www.bing.com/images/search?view=detailV2&ccid=DBwU%2frDU&id=C893ED626A8682FC9AF860374F39DF50CA7B5A69&thid=OIP.DBwU_rDUPkrBoukidKV6jAHaEo&mediaurl=https%3a%2f%2fi.pinimg.com%2foriginals%2ffd%2fde%2fb0%2ffddeb0ead5a0837214ac0c9ab5616990.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.0c1c14feb0d43e4ac1a2e92274a57a8c%3frik%3daVp7ylDfOU83YA%26pid%3dImgRaw%26r%3d0&exph=2400&expw=3840&q=imagen+tanke&simid=608015345027846143&FORM=IRPRST&ck=CAAB0561B8B802547E12AEA8F19AD0BE&selectedIndex=0");
                         imagen.post(new Runnable() {
                             @Override
                             public void run() {
